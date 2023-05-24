964bb9a2baacfe7c69f16e26351f8624c34aefd6
Dedeyd70 <128816014+Dedeyd70@users.noreply.github.com>
2023-05-24 21:00:31 +0000

    Update abena.c

--- abena.c
+++ abena.c
@@ -1,1 +1,14 @@
 abena is a girl
+#include <string.h>
+
+/**
+* main - entry points
+* @dede: the girl
+* @adam: the dog
+* return: always 0(success)
+*/
+int main(void)
+{
+  for (s = 0; s < size; s++)
+    return (0);
+}
